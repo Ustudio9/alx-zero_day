6cb3ba45b9bd7dfa79817bae62186b108952b1ca
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-08 16:06:24 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-/*
+/**
+ * main - print the string in the put function
  *
- * main - Entry point
- *
- * Description: a C program that prints exactly "Programming
- * is like building a multilingual puzzle, followed by a new line
+ * Description: using the main function
+ * building a multilingual puzzle, followed by a new line
  *
  * Return: Always 0 (Success)
- *
  */
-
 int main(void)
 
 {

3a2a4ec6b4dd2396e2547efb3ad1eaf0f98aa37e
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-08 15:53:07 -0700

    add

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /*
  *
- * main: void
- * Description - C program that prints exactly with
+ * main - Entry point
+ *
+ * Description: C program that prints exactly with
  * proper grammar, but the outcome is a piece of art,
  *
- * Return: 0
+ * Return: Always 0 (Success)
 */
 int main(void)
 {

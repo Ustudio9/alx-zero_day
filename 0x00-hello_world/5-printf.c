b717e0869478160291f186c9ef664993b706c727
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-08 15:08:02 -0700

    add

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * C program that prints exactly with proper grammar, but the outcome is a piece of art, 
+ * using printf
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

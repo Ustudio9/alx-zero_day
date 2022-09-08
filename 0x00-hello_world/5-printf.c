f7df5d4d69f872fd2902a99b49af57417049f236
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-08 15:38:29 -0700

    add

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,12 @@
 /*
  *
  * main - print the string in the function
- * Description - C program that prints exactly with 
+ * Description - C program that prints exactly with
  * proper grammar, but the outcome is a piece of art,
  *
  * Return: Always 0 (Success)
 */
-
-int main(void) /* return void */
-
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 

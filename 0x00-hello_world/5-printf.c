7c648abe48b7dfb4924ab7c718bcebd099bb6922
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-08 15:11:52 -0700

    add

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
-/*
- * C program that prints exactly with proper grammar, but the outcome is a piece of art, 
- * using printf
- */
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+	
 {
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
-}
+
+}}

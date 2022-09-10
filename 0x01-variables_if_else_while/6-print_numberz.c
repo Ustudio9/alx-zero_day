a8318d009aa9525890a24ed26230bd09c0237395
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-09 17:19:38 -0700

    ok

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - prints all single digit numbers of based 10 starting from 0,
  * followed by a new line, using putchar
  * Return: ALway 0 (Success)
  */
+int main(void)
 {
 	int n;
 

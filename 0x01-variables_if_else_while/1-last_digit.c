22b26b65dc0395a908dd3ca84e19a893e47178cd
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-10 05:40:48 -0700

    add

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,8 +11,7 @@
 int main(void)
 {
 	int n;
-	
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is %d", n, n % 10);
@@ -20,11 +19,11 @@ int main(void)
 	{
 		printf("and is greater than 5\n");
 	}
-	if (n % 10 == 0)
+	else if (n % 10 == 0)
 	{
 		printf("and is 0\n");
 	}
-	if (n % 10 < 6 && n != 0)
+	else if (n % 10 < 6 && n % 10 != 0)
 	{
 		printf("and is less than 6 and not 0\n");
 	}

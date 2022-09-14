c145913788f0a50d12b335dc4e7100c4e4eed9ac
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-14 02:17:44 -0700

    add

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/** 
+ * _isalpha - a function that checks for alphabetic character.
+ * @c: is a letter, lowercase or uppercase
+ * Return: 0 (Success)
+ */
+int _isalpha(int c)
+{
+
+	if (c = 97 && c <= 122)
+	else if (c = 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}

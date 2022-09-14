90a517d9e497e4e68ca486f858fc89ff505c4734
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-14 01:03:58 -0700

    add

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - a function that prints 10 times the alphabet, in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+	int n;
+	int b;
+
+	while(n = 0; n <= 9; n++)
+	{
+		for (b = 'a'; b <= z; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}

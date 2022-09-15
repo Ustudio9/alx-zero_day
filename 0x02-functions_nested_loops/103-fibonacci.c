701222f605d9dd950d2ea711838d1d2c65b7bfe8
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-15 14:51:37 -0700

    ok

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,25 +7,17 @@
  */
 int main(void)
 {
-	int i;
-	unsigned long int j, k, sum, next;
+	int a = 0, b = 1, next = 0;
+	int sum = 0;
 
-	j = 1;
-	k = 2;
-	sum = 0;
-
-	sum = 0;
+	while (next < 4000000)
 	{
-		if (j % 4000000 && (j % 2) == 0)
-		{
-			sum = sum + j;
-		}
-		next = j + k;
-		j = k;
-		k = next;
-
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
 	}
-	printf("%lu\n", sum);
-
+	printf("%i\n", sum);
 	return (0);
 }

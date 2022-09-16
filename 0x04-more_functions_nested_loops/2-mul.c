7e4f9c3b3fefbc8e75fde3cebe585528d9a3bc3b
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-15 17:19:17 -0700

    ok

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -9,21 +9,11 @@
  * */
 int mul(int a, int b)
 {
-	return (a * b);
-}
-
-int main()
-{
 	int a, b, prod;
-
-	printf("Enter the first number: ");
-	scanf("%d", &a);
-	printf("Enter the second number: ");
-	scanf("%d", &b);
 
 	prod = mul(a, b);
 
 	printf("Product of %d and %d is: %d", a, b, prod);
 
-	return 0;
+	return (a * b);
 }

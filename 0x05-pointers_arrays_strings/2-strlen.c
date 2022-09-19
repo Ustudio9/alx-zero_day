e34b84352c1bebb3126c06b7d3f410092d905ad1
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-19 04:39:32 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+
+/**
+ * _strlen - Prints the len of a string
+ * @s: The memory address of string
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+
+int _strlen(char *s)
+
+{
+
+int k = strlen(s);
+
+return (k);
+
+}

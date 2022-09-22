6605b0f678ddcdce4ff3a02d8bd9cb19e959bd16
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-09-22 12:58:08 -0700

    ok

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/*
+#include <stdio.h>
+/**
  * print_buffer - a function that prints a buffer.
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
